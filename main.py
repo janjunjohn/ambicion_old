93f2bc63c36457e1129d3bb54ae72a877f9c4e9e
Jun Kakishima <adventureocean3699@gmail.com>
2022-02-27 02:28:33 +0900

    add test message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 from flask import Flask, render_template, request
-import sendgrid
+from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from email.mime.text import MIMEText
 import os
@@ -38,26 +38,19 @@ def order():
         message = request.form['free-text']
 
         charset = 'iso-2022-jp'
-        subject = MIMEText(f'{username} 様', charset)
-        contents = MIMEText(f'お名前: {username} \n'
-                            f'Email: {user_email} \n'
-                            f'ユニフォーム: {uniform} {socks} \n'
-                            f'枚数: {number} \n'
-                            f'生地: {fabric} \n'
-                            f'襟: {neck} \n'
-                            f'袖: {sleeve} \n'
-                            f'備考: {message} \n',
-                            'plain',
-                            charset
-                            )
+        subject = '件名です'
+        contents = 'こんにちは'
+
+
         message = Mail(
             from_email=PYTHON_EMAIL,
             to_emails=AMBICION_EMAIL,
             subject=subject,
             plain_text_content=contents
         )
-        sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_APIKEY)
-        sg.send(message)
+        sg = SendGridAPIClient(api_key=SENDGRID_APIKEY)
+        response = sg.send(message)
+        print(response.status_code)
 
         return render_template('order_page.html', submitted=True, username=username)
 
